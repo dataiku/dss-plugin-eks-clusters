f6abdd585904d77326ffc1bb14d98ba9035e42ba
gscase <gscase>
2021-06-06 03:08:42 +0000

    Edited files

--- python-lib/dku_aws/boto3_command.py
+++ python-lib/dku_aws/boto3_command.py
@@ -1,14 +1,15 @@
 import sys, os, subprocess, logging, json, requests, shutil, boto3, json, tabulate, urllib.request, re, math
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
 from dku_utils.access import _has_not_blank_property
 
-def get_dss_instance_variables ()
+
+def get_dss_instance_variables ():
     ec2_client = boto3.client('ec2')
     dss_region = ec2_client.meta.region_name
     current_date_time = datetime.now()
-    strt_dt_window = current_date_time + datetime.timedelta(-31)
+    strt_dt_window = current_date_time + timedelta(days = -31)
 
     """
     Get the DSS Instance ID. Will use this to track back to the availability zone of the instance.
@@ -30,6 +31,6 @@ def get_dss_instance_variables ()
             
     d = {}
     
-    d = {'instance_id': dss_instance_id, 'region': dss_region, 'availability_zone': dss_availability_zone, 'current_dt': current_data_time, 'strt_dt_window': strt_dt_window}
+    d = {'instance_id': dss_instance_id, 'region': dss_region, 'availability_zone': dss_availability_zone, 'current_dt': current_date_time, 'strt_dt_window': strt_dt_window}
     
     return d

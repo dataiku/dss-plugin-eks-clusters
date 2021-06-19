30e89dc6b4c908db19e9a80a4297beb9406c9189
gscase <gscase>
2021-06-19 05:35:56 +0000

    Edited files

--- resource/get_instance_types.py
+++ resource/get_instance_types.py
@@ -1,19 +1,22 @@
 def do(payload, config, plugin_config, inputs):
-    from dku_aws.boto3_command import get_instances_and_spot 
+    from dku_aws.boto3_command import get_instances_and_spot, split_fsg 
     
+    instanceFamily = plugin_config.get("instanceFamily")
     instanceVCPUsMin = plugin_config.get("instanceVCPUsMin")
     instanceVCPUsMax = plugin_config.get("instanceVCPUsMax")
     memoryMin = plugin_config.get("memoryMin")
     memoryMax = plugin_config.get("memoryMax")
 
+
     instances_df = get_instances_and_spot()
-    instances_df = instances_df[['Instance_Type','vCPUs','Memory','GPU_Ind','Processor_Speed','Current_Spot_Price','Instance_Recommended']]
+    instances_df = instances_df[['Instance_Type','vCPUs','Memory','GPU_Ind','Processor_Speed','Current_Spot_Price','Instance_Recommended', 'Instance_Family']]
     instances_df = instances_df.sort_values(by=['Instance_Recommended', 'GPU_Ind', 'vCPUs', 'Memory', 'Processor_Speed', 'Current_Spot_Price'], ascending=[False, True, True, False, False, True])
 
     instances_df = instances_df[(instances_df.vCPUs >= instanceVCPUsMin) & 
                                 (instances_df.vCPUs <= instanceVCPUsMax) &
                                 (instances_df.Memory >= memoryMin) &
-                                (instances_df.Memory <= memoryMax)
+                                (instances_df.Memory <= memoryMax) & 
+                                (instances_df.Instance_Family.isin(instanceFamily))
                                
                                ]
 

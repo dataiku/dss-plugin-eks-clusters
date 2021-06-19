dcb4f2cf838ccf4de1ed62b7eaa58212654b33da
Grant Case <hangtime79@gmail.com>
2021-06-19 11:36:29 +1000

    first plugin_config pass

--- resource/get_instance_types.py
+++ resource/get_instance_types.py
@@ -1,10 +1,19 @@
 def do(payload, config, plugin_config, inputs):
     from dku_aws.boto3_command import get_instances_and_spot
 
-    
+
+    self.plugin_config = plugin_config
+
+    instanceVCPUsMin = self.config.plugin_config('instanceVCPUsMin', {})
+    instanceVCPUsMax = self.config.plugin_config('instanceVCPUsMax', {})
+    memoryMin = self.config.plugin_config('memoryMin', {})
+    memoryMax = self.config.plugin_config('memoryMax', {})
+
     instances_df = get_instances_and_spot()
     instances_df = instances_df[['Instance_Type','vCPUs','Memory','GPU_Ind','Processor_Speed','Current_Spot_Price','Instance_Recommended']]
     instances_df = instances_df.sort_values(by=['Instance_Recommended', 'GPU_Ind', 'vCPUs', 'Memory', 'Processor_Speed', 'Current_Spot_Price'], ascending=[False, True, True, False, False, True])
+
+    instances_df = instances_df[instances_df.vCPUs > ]
 
     instances_df['Inst_Description'] = (
         instances_df['Instance_Type'] + '/' + 

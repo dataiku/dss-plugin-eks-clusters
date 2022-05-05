8880b6223cbf5f86791f7dae6c26cd39802770fb
Alexandre Checinski <alexandre.checinski@dataiku.com>
2022-05-05 17:36:28 +0200

    [SC-82495] surrounding metrics server installation with a try/except (failure should not prevent cluster from being created).

--- python-lib/dku_kube/metrics_server.py
+++ python-lib/dku_kube/metrics_server.py
@@ -1,9 +1,13 @@
-import os, json, logging
-from .kubectl_command import run_with_timeout
+import os, json, logging, traceback
+from .kubectl_command import run_with_timeout, KubeCommandException
 
 def install_metrics_server(kube_config_path):
-    env = os.environ.copy()
-    env['KUBECONFIG'] = kube_config_path
-    cmd = ['kubectl', 'apply', '-f', 'https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml']
-    logging.info("Installing Metrics Server with : %s" % json.dumps(cmd))
-    out, err = run_with_timeout(cmd, env=env, timeout=30)
+    try:
+        env = os.environ.copy()
+        env['KUBECONFIG'] = kube_config_path
+        cmd = ['kubectl', 'apply', '-f', 'https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml']
+        logging.info("Installing Metrics Server with : %s" % json.dumps(cmd))
+        out, err = run_with_timeout(cmd, env=env, timeout=30)
+    except KubeCommandException as e:
+        traceback.print_exc()
+        logging.warning('Failed to install metrics server: %s' % json.dumps([cmd, e.rv, e.out, e.err]))

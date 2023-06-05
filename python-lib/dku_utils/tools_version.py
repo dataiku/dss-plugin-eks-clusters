96bcd839e4ed54f789def14b3b71d6b571e4cd8c
Amandine Souilleux <amandine.souilleux@dataiku.com>
2023-06-05 11:12:50 +0200

    [sc-138603] add robustness

--- python-lib/dku_utils/tools_version.py
+++ python-lib/dku_utils/tools_version.py
@@ -9,11 +9,15 @@ def get_kubectl_version():
 def get_kubectl_version_int(kubectl_version):
     # the kubectl version downloaded from Amazon website has a minor version finishing by '+'
     # keeping only the first numeric sequence for the minor version
-    regex_minor_int = re.compile("^[^0-9]*([0-9]+)")
+    if 'major' not in kubectl_version or 'minor' not in kubectl_version:
+        raise Exception("Kubectl version found on the machine is not correctly formatted")
+    regex_minor_int = re.compile("^[^0-9]*([0-9]+)([^0-9].*$|$)")
     search_results_minor_int = re.search(regex_minor_int, kubectl_version['minor'])
-    if len(search_results_minor_int.groups()) < 1:
-        raise Exception("Kubectl version found on the machine: %s. It was not possible to parse" % (str(kubectl_version['major']) + '.' + str(kubectl_version['minor']))
-    minor_int = int(search_results_minor_int.group(0))
+    if not search_results_minor_int:
+        raise Exception("No kubectl minor version found")
+    if not search_results_minor_int.groups():
+        raise Exception("Kubectl version found on the machine: %s. It was not possible to parse" % (str(kubectl_version['major']) + '.' + str(kubectl_version['minor'])))
+    minor_int = int(search_results_minor_int.groups()[0])
     return int(kubectl_version['major']), minor_int
 
 def get_authenticator_version():

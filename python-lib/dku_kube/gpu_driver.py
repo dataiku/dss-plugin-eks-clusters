71fee15874428bf7f9e50163ae2d22f583aa3473
Valentin Rutz <val.rutz@dataiku.com>
2024-03-29 11:19:44 +0100

    Download tolerations from daemonset and merge them with plugin config and node group taints

--- python-lib/dku_kube/gpu_driver.py
+++ python-lib/dku_kube/gpu_driver.py
@@ -1,6 +1,7 @@
 import os, json, logging, requests, yaml
 
 from dku_aws.eksctl_command import EksctlCommand
+from dku_utils.access import _is_none_or_blank
 from .kubectl_command import run_with_timeout
 
 def has_gpu_driver(kube_config_path):
@@ -12,34 +13,42 @@ def has_gpu_driver(kube_config_path):
     return len(out.strip()) > 0
 
 def add_gpu_driver_if_needed(cluster_id, kube_config_path, connection_info, taints):
-    if not has_gpu_driver(kube_config_path) and not check_eksctl_version(connection_info):
-        nvidia_config_raw = requests.get('https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/main/nvidia-device-plugin.yml').text
-    else:
-        cmd = ['kubectl', 'describe', 'daemonset']
-        nvidia_config_raw, err = run_with_timeout(cmd, env=env, timeout=5)
-
-    local_nvidia_plugin_config = os.path.join(os.environ["DIP_HOME"], 'clusters', cluster_id, 'nvidia-device-plugin.yml')
+    # Get the Nvidia driver plugin configuration from the repository
+    nvidia_config_raw = requests.get('https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/main/nvidia-device-plugin.yml').text
     nvidia_config = yaml.safe_load(nvidia_config_raw)
+    tolerations = set()
 
-    if taints and nvidia_config.get('spec', {}) and nvidia_config['spec'].get('template', {}) and nvidia_config['spec']['template'].get('spec', {}):
-        tolerations = nvidia_config['spec']['template']['spec'].get('tolerations', [])
+    # Get any tolerations from the plugin configuration
+    if nvidia_config.get('spec', {}) and nvidia_config['spec'].get('template', {}) and nvidia_config['spec']['template'].get('spec', {}):
+        tolerations = set(nvidia_config['spec']['template']['spec']['tolerations'])
 
-        # If there are any taints to patch the daemonset with,
-        # We add them to the GPU plugin configuration before updating with another `kubectl apply`
-        for taint in taints:
-            # Add the relevant toleration operator
-            if taint.get('value', ''):
-                taint['operator'] = 'Equal'
-            else:
-                taint['operator'] = 'Exists'
+    # Retrieve the tolerations on the daemonset currently deployed to the cluster.
+    if has_gpu_driver(kube_config_path):
+        cmd = ['kubectl', 'get', 'daemonset', 'nvidia-device-plugin-daemonset', '-n', 'kube-system', '-o', 'jsonpath="{.spec.template.spec.tolerations}"']
+        tolerations_raw, err = run_with_timeout(cmd, env=env, timeout=5)
+        if _is_none_or_blank(tolerations_raw):
+            tolerations = tolerations.update(json.loads(tolerations_raw))
 
-            # If the toleration is not in the list, add it
-            if taint in tolerations:
-                tolerations.append(taint)
+    # If there are any taints to patch the daemonset with in the node group(s) to create,
+    # we add them to the GPU plugin configuration before updating with another `kubectl apply`
+    for taint in taints or []:
+        # Add the relevant toleration operator
+        if taint.get('value', ''):
+            taint['operator'] = 'Equal'
+        else:
+            taint['operator'] = 'Exists'
 
+        # If the toleration is not in the set, add it
+        if taint not in tolerations:
+            tolerations.add(taint)
+    nvidia_config['spec']['template']['spec']['tolerations'] = list(tolerations)
+
+    # Write the configuration locally
+    local_nvidia_plugin_config = os.path.join(os.environ["DIP_HOME"], 'clusters', cluster_id, 'nvidia-device-plugin.yml')
     with open(local_nvidia_plugin_config, "w") as f:
         yaml.safe_dump(nvidia_config, f)
 
+    # Apply the patched Nvidia driver configuration to the cluster
     cmd = ['kubectl', 'apply', '-f', local_nvidia_plugin_config]
     logging.info('Running command to install Nvidia drivers: %s', ' '.join(cmd))
     logging.info('NVIDIA GPU driver config: %s' % yaml.safe_dump(nvidia_config, default_flow_style=False))
